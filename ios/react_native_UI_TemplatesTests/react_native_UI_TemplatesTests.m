f21747930dca3ae89bb22cea20264a22ad6abd34
Ashutosh <asutosh.dubey97@gmail.com>
2023-04-22 19:14:03 +0530

    chore: upgrade to react-native v0.71.7

--- ios/react_native_UI_TemplatesTests/react_native_UI_TemplatesTests.m
+++ ios/react_native_UI_TemplatesTests/react_native_UI_TemplatesTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface react_native_UI_TemplatesTests : XCTestCase
+@interface react_native_ui_templatesTests : XCTestCase
 
 @end
 
-@implementation react_native_UI_TemplatesTests
+@implementation react_native_ui_templatesTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
